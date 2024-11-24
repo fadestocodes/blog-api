6d665df360de32862d6361a05900dcb7959dcbc9
Andrew <andrew-jung@hotmail.com>
2024-11-24 11:28:18 -0800

    Update vite.config to handle SSR AND CSR

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,10 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build : {
-    ssr : 'src/entry-server.jsx',
-    rollupOptions : '/index.html'
+export default defineConfig(({mode}) =>{
+  const isSSR = mode === 'ssr';
+  return {
+    plugins : [react()],
+    build : {
+      ssr : isSSR ? './src/entry-server.jsx' : false,
+      outDir : isSSR ? './dist/server' : './dist/client',
+      rollupOptions : {
+        input : isSSR ? './src/entry-server.jsx' : './index.html'
+      }
+    }
   }
-});
+})
